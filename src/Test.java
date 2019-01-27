41e7ed87723bb2278669b015f68de8e27c33d714
venky <gvenkateswarlu910116@gmail.com>
2019-01-27 23:59:18 +0530

    Second commit to TopStockTwo

--- src/Test.java
+++ src/Test.java
@@ -3,9 +3,9 @@ import org.codehaus.jackson.map.ObjectMapper;
 public class Test {
 	public static void main(String[] args) {
 		Employee e=new Employee();
-		e.setEmpId(100);
-		e.setEmpName("AAA");
-		e.setEmpSal(6600.36);
+		e.setEmpId(1000);
+		e.setEmpName("AAAA");
+		e.setEmpSal(66000.36);
 		try {
 			ObjectMapper om=new ObjectMapper();
 			String json=om.writeValueAsString(e);
